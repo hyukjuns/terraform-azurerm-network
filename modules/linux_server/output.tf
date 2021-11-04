602a1beb62c32ecd53aaae6573dad92db109fbfd
namhj94 <hyukjun1994@gmail.com>
2021-11-04 18:32:13 +0900

    init modules

---
+++ modules/linux_server/output.tf
@@ -0,0 +1,9 @@
+output "linux_vm_private_ip" {
+    value = azurerm_linux_virtual_machine.linux_vm.private_ip_address
+}
+output "linux_vm_public_ip" {
+    value = azurerm_linux_virtual_machine.linux_vm.public_ip_address
+}
+output "nic_id" {
+    value = azurerm_network_interface.linux_vm.id
+}
